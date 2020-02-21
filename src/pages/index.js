b5d6a26c35fb20c50bdfdd75c04dce9d16501621
defazio <michael.defazio@elastic.co>
2020-02-21 18:40:12 -0500

    Add css version of elastic logo animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import LogoAnimationA from '../components/logo';
+import LoadingElastic from '../components/loading-elastic';
 import { EuiPage, EuiPageBody } from '@elastic/eui';
 
 export default () => (
   <EuiPage restrictWidth>
     <EuiPageBody>
-      <LogoAnimationA />
+      <div style={{ width: 150, margin: '100px auto' }}>
+        <LoadingElastic />
+      </div>
     </EuiPageBody>
   </EuiPage>
 );
